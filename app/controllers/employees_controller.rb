aafdb8074dee4c3dbaa1d9ee2f0059cc1ec1bd28
danizaghian <dani.zaghian@gmail.com>
2017-09-26 20:47:12 -0700

    added update and delete actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -21,4 +21,23 @@ class EmployeesController < ApplicationController
     render 'show.json.jbuilder'
   end
 
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      ssn: params[:ssn],
+      birthdate: params[:birthdate]
+    )
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    @employees = Employee.all
+    render 'index.json.jbuilder'
+  end
+
 end
